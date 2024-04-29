f351d9c14f871ba95c4290f084620f9b0cde87b0
Danny Idt <idtdanny@gmail.com>
2024-04-29 15:16:21 +0000

    0. Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author IdtDanny <https://github.com/IdtDanny>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
